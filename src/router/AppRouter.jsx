1a0531513e2ac287aff1fed958b814d3c7bddfee
Mehefuj Ali <mehefujalim@gmail.com>
2025-01-14 10:58:41 +0530

    react app created with react router and tailwind css

---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,11 @@
+import { createBrowserRouter } from "react-router-dom";
+
+
+const AppRouter = createBrowserRouter([
+      {
+            path:'/',
+            element: <h1>Hello i am home</h1>
+      }
+])
+
+export default AppRouter;
