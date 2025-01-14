129b388a296c79708c9bb017a9c753e0fd6563ca
Mehefuj Ali <mehefujalim@gmail.com>
2025-01-14 15:20:16 +0530

    trending news fatch and swiper js viewed

--- src/components/TrendingSwiper/TrendingSwiper.jsx
+++ src/components/TrendingSwiper/TrendingSwiper.jsx
@@ -3,34 +3,65 @@ import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
 import "swiper/css";
-import './trendingswiper.css'
+import "./trendingswiper.css";
 import "swiper/css/pagination";
-import { Pagination } from "swiper/modules";
+import { Autoplay, Pagination } from "swiper/modules";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import { Button } from "@material-tailwind/react";
 const TrendingSwiper = () => {
+  const [trendingNews, setTrendingNews] = useState([]);
+  useEffect(() => {
+    axios.get("http://localhost:8080/articles").then((res) => {
+      setTrendingNews(res.data);
+    });
+  }, []);
+
   return (
-    <div className=" container mx-auto my-14">
+    <div className=" container mx-auto my-14 w-11/12 lg:w-full">
       <Swiper
-        slidesPerView={3}
+        breakpoints={{
+           
+            320: {
+              slidesPerView: 1,
+              spaceBetween: 10,
+            },
+           
+            768: {
+              slidesPerView: 2,
+              spaceBetween: 20,
+            },
+           
+            1024: {
+              slidesPerView: 3,
+              spaceBetween: 30,
+            },
+          }}
         spaceBetween={60}
         pagination={{
           clickable: true,
         }}
-        modules={[Pagination]}
+        loop={true}
+        modules={[Pagination, Autoplay]}
         className="mySwiper"
+        autoplay={{
+          delay: 4000,
+        }}
       >
-        <SwiperSlide><div className="  bg-red-500">
-            1</div></SwiperSlide>
-        <SwiperSlide><div className=" bg-red-500">
-            2</div></SwiperSlide>
-        <SwiperSlide><div className="  bg-red-500">
-           3 </div></SwiperSlide>
-        <SwiperSlide><div className=" bg-red-500">
-            4</div></SwiperSlide>
-        <SwiperSlide><div className=" bg-red-500">
-            5</div></SwiperSlide>
-        <SwiperSlide><div className="  bg-red-500">
-           6 </div></SwiperSlide>
-        
+        {trendingNews.map((news) => (
+          <SwiperSlide key={news._id}>
+            <div className=" text-start shadow-md  ">
+              <img className=" max-h-52 w-full" src={news?.image} alt="" />
+              <div className="p-6 flex flex-col gap-2">
+                <h3 className=" font-semibold">{news?.title}</h3>
+                <p className=" text-nowrapa ">{news?.publisher}</p>
+                <p className=" text-sm">{news?.description.slice(0,140)}...</p>
+                <Link><Button className=" rounded bg-primary-color">See details</Button></Link>
+              </div>
+            </div>
+          </SwiperSlide>
+        ))}
       </Swiper>
     </div>
   );
