7732104d0a60dff471a63b92122446bda94a0a46
Mehefuj Ali <mehefujalim@gmail.com>
2025-01-23 21:31:08 +0530

    jwt created

--- src/Context/AuthProvider.jsx
+++ src/Context/AuthProvider.jsx
@@ -10,25 +10,47 @@ import {
 import PropTypes from "prop-types";
 import { createContext, useEffect, useState } from "react";
 import { auth } from "../Firebase/firebase.init";
+import useAxiosPublic from "./../Hooks/useAxiosPublic";
 
 const AuthContext = createContext();
 
 const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
+  const axiosPublic = useAxiosPublic();
   const googleProvider = new GoogleAuthProvider();
   const SininWithGoogle = () => {
     return signInWithPopup(auth, googleProvider);
   };
+
   useEffect(() => {
     const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
-      setUser(currentUser);
-      setLoading(false);
+      if (currentUser) {
+        //get token and store clinent
+        const userInfo = {
+          email: currentUser.email,
+        };
+        axiosPublic.post("/jwt", userInfo).then((res) => {
+          if (res.data.token) {
+            localStorage.setItem("access-token", res.data.token);
+            setUser(currentUser);
+            setLoading(false);
+          }
+        });
+      } else {
+        localStorage.removeItem("access-token");
+        setUser(currentUser);
+        setLoading(false);
+      }
     });
     return () => {
       unSubscribe();
     };
   });
+
+
+  //////////////////////////////
+
   const signOutUser = () => {
     return signOut(auth);
   };
@@ -38,9 +60,9 @@ const AuthProvider = ({ children }) => {
   const updateUser = (updateDoc) => {
     return updateProfile(auth.currentUser, updateDoc);
   };
-  const emailLogin = (email,password) => {
-    return signInWithEmailAndPassword(auth,email,password)
-  }
+  const emailLogin = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password);
+  };
 
   const authInfo = {
     SininWithGoogle,
--- src/Hooks/useAxiosSecure.jsx
+++ src/Hooks/useAxiosSecure.jsx
@@ -1,10 +1,36 @@
 import axios from "axios";
+import useAuth from "./useAuth";
 
 const axiosSecure = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
 });
 
 const useAxiosSecure = () => {
+  const { signOutUser } = useAuth();
+  axiosSecure.interceptors.request.use(
+    function (config) {
+      const token = localStorage.getItem("access-token");
+      config.headers.authorization = `Bearer ${token}`;
+      return config;
+    },
+    function (error) {
+      return Promise.reject(error);
+    }
+  );
+
+  axiosSecure.interceptors.response.use(
+    function (res) {
+      return res;
+    },
+    (err) => {
+      const status = err.response.status;
+      if (status === 401 || status === 403) {
+        signOutUser();
+      }
+
+      return Promise.reject(err);
+    }
+  );
   return axiosSecure;
 };
 
